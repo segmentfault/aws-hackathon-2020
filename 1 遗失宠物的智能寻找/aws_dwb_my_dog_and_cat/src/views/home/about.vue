537a007b835d33d7bdf248631cb93668426ffb1c
gdoudeng <gdoudeng@qq.com>
2020-09-11 19:21:13 +0800

    chore: 提交代码

---
+++ 1 遗失宠物的智能寻找/aws_dwb_my_dog_and_cat/src/views/home/about.vue
@@ -0,0 +1,100 @@
+<!-- home -->
+<template>
+  <div class="about-container">
+    <div class="warpper">
+      <div class="list">
+        <div class="logo"></div>
+        <div class="demo-home__title">宠物识别项目</div>
+        <div class="item">项目作者: dwb</div>
+        <div class="item"></div>
+        <div class="item">使用到的AWS技术</div>
+        <div class="item">
+          {{ userName }}
+          <van-button v-if="userName == ''" type="warning" size="small" @click="doDispatch">快点我~</van-button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import {mapGetters} from 'vuex'
+
+export default {
+  data() {
+    return {
+      wechat: `${this.$cdn}/wx/640.gif`
+    }
+  },
+  computed: {
+    ...mapGetters(['userName'])
+  },
+  methods: {
+    // Action 通过 store.dispatch 方法触发
+    doDispatch() {
+      this.$store.dispatch('setUserName', 'Amazon S3、 Amazon SageMaker、 aws-sdk-js')
+    },
+  }
+}
+</script>
+<style lang="scss">
+.about-container {
+  /* 你的命名空间 */
+  background: #fff;
+  height: 100vh;
+  box-sizing: border-box;
+
+  .warpper {
+    padding: 50px 12px 12px 12px;
+
+    .list {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      color: #666;
+      font-size: 14px;
+
+      .demo-home__title {
+        margin: 0 0 6px;
+        font-size: 32px;
+
+        .demo-home__title img,
+        .demo-home__title span {
+          display: inline-block;
+          vertical-align: middle;
+        }
+      }
+
+      .item {
+        font-size: 14px;
+        line-height: 34px;
+
+        a {
+          text-decoration: underline;
+        }
+
+        .van-button {
+          /* vant-ui 元素*/
+          background: #ff5500;
+        }
+      }
+
+      .logo {
+        width: 120px;
+        height: 120px;
+        background: url($cdn+ '/weapp/logo.png') center / contain no-repeat;
+      }
+
+      .wechat {
+        width: 200px;
+        height: 200px;
+
+        img {
+          width: 100%;
+          height: auto;
+        }
+      }
+    }
+  }
+}
+</style>
---
+++ 1 遗失宠物的智能寻找/aws_dwb_my_dog_and_cat/src/views/home/index.vue
@@ -0,0 +1,67 @@
+<!-- home -->
+<template>
+  <div class="index-container">
+    <div class="warpper">
+      <h1 class="demo-home__title"><img src="https://imgs.solui.cn/weapp/logo.png"/><span> 宠物识别</span></h1>
+      <h2 class="demo-home__desc">
+        使用到的AWS技术
+      </h2>
+    </div>
+    <van-cell icon="success" v-for="item in list" :key="item" :title="item"/>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      list: [
+        'Amazon S3',
+        'Amazon SageMaker',
+        'aws-sdk-js',
+      ]
+    }
+  },
+
+  computed: {},
+
+  mounted() {
+  },
+
+  methods: {}
+}
+</script>
+<style lang="scss" scoped>
+.index-container {
+  .warpper {
+    padding: 12px;
+    background: #fff;
+
+    .demo-home__title {
+      margin: 0 0 6px;
+      font-size: 32px;
+
+      .demo-home__title img,
+      .demo-home__title span {
+        display: inline-block;
+        vertical-align: middle;
+      }
+
+      img {
+        width: 32px;
+      }
+
+      span {
+        margin-left: 16px;
+        font-weight: 500;
+      }
+    }
+
+    .demo-home__desc {
+      margin: 0 0 20px;
+      color: rgba(69, 90, 100, 0.6);
+      font-size: 14px;
+    }
+  }
+}
+</style>

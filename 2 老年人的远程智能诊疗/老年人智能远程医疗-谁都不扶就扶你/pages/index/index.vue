d671fd3d9f34bedc0ca4f7ab0fa5e11df88a13e9
z221 <71182518+z221@users.noreply.github.com>
2020-09-12 22:32:42 +0800

    Add files via upload

---
+++ 2 老年人的远程智能诊疗/老年人智能远程医疗-谁都不扶就扶你/pages/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view>
+			
+	</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+		return {
+				PageCur: 'basics'
+			}
+		},
+		methods: {
+			NavChange: function(e) {
+				this.PageCur = e.currentTarget.dataset.cur
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
